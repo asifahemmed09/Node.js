6cff6644faf5bab0f255a1699f3d5ca46b0d882f
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-07 05:00:18 +0600

    import json module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const add = require('./add');
 const user = require('./user');
 const math = require('./math');
+const data = require('./data')
 
 console.log('Hello from index.js');
 
@@ -16,3 +17,8 @@ console.log(newUser.getName()); //* Smith because module cached
 
 console.log(math.add(2, 3));
 console.log(math.subtract(3, 2));
+
+
+console.log(data)
+console.log(data.name)
+console.log(data.address)
