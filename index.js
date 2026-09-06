27ee3a893b98ae14195b387e89807a9f300744a0
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-07 03:10:40 +0600

    module caching

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const add = require("./add")
+const user = require("./user")
 
 console.log("Hello from index.js")
 
 const sum = add(2,2)
 console.log(sum)
+
+console.log(user.getName())
+user.setName("Smith")
+console.log(user.getName())
+
+const newUser = require("./user")
+console.log(newUser.getName()) //* Smith because module cached 
+
