ee55183c0facc8b6919c0f029a08b5fd2704b50e
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-09 01:32:40 +0600

    http routing

--- http_module.js
+++ http_module.js
@@ -1,15 +1,25 @@
-const http = require("http")
+const http = require('http');
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
   const user = {
-    name: "Bruce Wayne",
+    name: 'Bruce Wayne',
     age: 24,
-    isMarried: false
+    isMarried: false,
+  };
+  if (req.url === '/') {
+    res.end('Home Page');
+  } else if (req.url === '/about') {
+    res.end('About Page');
+  } else if (req.url === '/user') {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(JSON.stringify(user));
   }
-  res.writeHead(200,{"content-type":"application/json"})
-  res.end(JSON.stringify(user))
-})
+  else{
+    res.writeHead(404)
+    res.end("Page not found")
+  }
+});
 
-server.listen(3000,()=>{
-  console.log("server is running on port: 3000")
-})
+server.listen(3000, () => {
+  console.log('server is running on port: 3000');
+});
